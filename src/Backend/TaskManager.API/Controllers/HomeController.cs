38d497dc32ef0530d6e9bc775abef03ce677acac
Farias <pablogarciafarias@gmail.com>
2025-01-18 17:55:51 -0300

    add healthcheck

---
+++ src/Backend/TaskManager.API/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.API.Controllers;
+
+[ApiController]
+[Route("")]
+public class HomeController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult HealthCheck()
+    {
+        return Ok("It's working!");
+    }
+}
